ac5ac6ef458dd4ecfc3af6728a8dd5737c5be1e9
igumus <ibrhm.gumus@gmail.com>
2023-12-19 01:12:23 +0300

    updated: access flag labels to lowercase

--- jinspect/src/flag.rs
+++ jinspect/src/flag.rs
@@ -5,41 +5,41 @@ use std::io::BufReader;
 use crate::reader;
 
 const ACC_CLASS: [(&str, u16); 8] = [
-    ("Public", 0x0001),
-    ("Final", 0x0010),
-    ("Super", 0x0020),
-    ("Interface", 0x0200),
-    ("Abstract", 0x0400),
-    ("Synthetic", 0x1000),
-    ("Annotation", 0x2000),
-    ("Enum", 0x4000),
+    ("public", 0x0001),
+    ("final", 0x0010),
+    ("super", 0x0020),
+    ("interface", 0x0200),
+    ("abstract", 0x0400),
+    ("synthetic", 0x1000),
+    ("annotation", 0x2000),
+    ("enum", 0x4000),
 ];
 
 const ACC_FIELD: [(&str, u16); 9] = [
-    ("Public", 0x0001),
-    ("Private", 0x0002),
-    ("Protected", 0x0004),
-    ("Static", 0x0008),
-    ("Final", 0x0010),
-    ("Volatile", 0x0040),
-    ("Transient", 0x0080),
-    ("Synthetic", 0x1000),
-    ("Enum", 0x4000),
+    ("public", 0x0001),
+    ("private", 0x0002),
+    ("protected", 0x0004),
+    ("static", 0x0008),
+    ("final", 0x0010),
+    ("volatile", 0x0040),
+    ("transient", 0x0080),
+    ("synthetic", 0x1000),
+    ("enum", 0x4000),
 ];
 
 const ACC_METHOD: [(&str, u16); 12] = [
-    ("Public", 0x0001),
-    ("Private", 0x0002),
-    ("Protected", 0x0004),
-    ("Static", 0x0008),
-    ("Final", 0x0010),
-    ("Synchronized", 0x0020),
-    ("Bridge", 0x0040),
-    ("Varargs", 0x0080),
-    ("Native", 0x0100),
-    ("Abstract", 0x0400),
-    ("Strict", 0x0800),
-    ("Synthetic", 0x1000),
+    ("public", 0x0001),
+    ("private", 0x0002),
+    ("protected", 0x0004),
+    ("static", 0x0008),
+    ("final", 0x0010),
+    ("synchronized", 0x0020),
+    ("bridge", 0x0040),
+    ("varargs", 0x0080),
+    ("native", 0x0100),
+    ("abstract", 0x0400),
+    ("strict", 0x0800),
+    ("synthetic", 0x1000),
 ];
 
 pub enum AccessFlag {
@@ -70,7 +70,7 @@ impl AccessFlag {
             .filter(|(_, mask)| item & *mask == *mask)
             .map(|(caption, _)| caption.to_string())
             .collect();
-        ret.join(";")
+        ret.join(" ")
     }
 
     pub fn to_string(&self) -> String {
