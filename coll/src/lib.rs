ff63231293ea0f11f721f1d230424fe9befa01f9
igumus <ibrhm.gumus@gmail.com>
2023-12-24 14:22:40 +0300

    added: iteratable feature to stack implementation

--- coll/src/lib.rs
+++ coll/src/lib.rs
@@ -8,6 +8,13 @@ struct Node<T> {
 
 type Link<T> = Option<Box<Node<T>>>;
 
+pub struct IntoIter<T>(Stack<T>);
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
 #[derive(Debug)]
 pub struct Stack<T> {
     head: Link<T>,
@@ -54,6 +61,22 @@ impl<T> Stack<T> {
             node.value
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
 }
 
 impl<T> Drop for Stack<T> {
@@ -62,6 +85,36 @@ impl<T> Drop for Stack<T> {
         while let Some(mut item) = current_link {
             current_link = item.next.take();
         }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.value
+        })
+    }
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.value
+        })
     }
 }
 
@@ -123,5 +176,65 @@ mod tests {
         }
         assert_eq!(stack.peek(), Some(&2));
         assert_eq!(stack.len(), 1);
+    }
+
+    #[test]
+    fn test_stack_into_iter() {
+        let mut stack = Stack::<i32>::new();
+        assert_eq!(stack.len(), 0);
+        assert_eq!(stack.peek_mut(), None);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        let mut iterator = stack.into_iter();
+        assert_eq!(iterator.next(), Some(5));
+        assert_eq!(iterator.next(), Some(4));
+        assert_eq!(iterator.next(), Some(3));
+        assert_eq!(iterator.next(), Some(2));
+        assert_eq!(iterator.next(), Some(1));
+        assert_eq!(iterator.next(), None);
+    }
+
+    #[test]
+    fn test_stack_iter() {
+        let mut stack = Stack::<i32>::new();
+        assert_eq!(stack.len(), 0);
+        assert_eq!(stack.peek_mut(), None);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        let mut iterator = stack.iter();
+        assert_eq!(iterator.next(), Some(&5));
+        assert_eq!(iterator.next(), Some(&4));
+        assert_eq!(iterator.next(), Some(&3));
+        assert_eq!(iterator.next(), Some(&2));
+        assert_eq!(iterator.next(), Some(&1));
+        assert_eq!(iterator.next(), None);
+    }
+
+    #[test]
+    fn test_stack_iter_mut() {
+        let mut stack = Stack::<i32>::new();
+        assert_eq!(stack.len(), 0);
+        assert_eq!(stack.peek_mut(), None);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        for item in stack.iter_mut() {
+            *item = 10;
+        }
+
+        for item in stack.iter() {
+            assert_eq!(item, &10);
+        }
     }
 }
